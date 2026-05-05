1a2376c306d3f9f46e74c2085cb83b45c8aead3c
Дом <dom@MacBook-Air-Dom.local>
2026-05-06 01:25:59 +0500

    Fixed PHP fatal error in brain-games, added hello function

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,6 @@ namespace BrainGames;
 use function cli\line;
 use function cli\prompt;
 
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
 function hello() {
     line('Welcome to the Brain Game!');
 
